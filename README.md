bb9a40966ebeab2256e8fdc2a684d0962cf16120
Daniel Bedoya <developerbedoya@gmail.com>
2021-05-25 10:44:52 -0300

    Add parameters to raspicam_timelapse_photo.sh

--- README.md
+++ README.md
@@ -4,14 +4,18 @@
 The purpose of this scripts is to take some photos from a Raspberry Pi and make a daily stop motion video
 
 ## Usage
+
 I'm using cron for taking photos each minute and making (and updating) the video each hour at minute 59
 
 ```
-* *   * * *   pi  /home/pi/repos/raspicam-timelapse/raspicam_timelapse_photo.sh
+* *   * * *   pi  /home/pi/repos/raspicam-timelapse/raspicam_timelapse_photo.sh -o /home/pi/shared/raspicam-timelapse -a greyworld -m 4
 59 *   * * *   pi  /home/pi/repos/raspicam-timelapse/raspicam_timelapse_video.sh
 ```
 
-Right now this is saving the pictures in `/home/pi/shared/raspicam-timelapse`, and the timelapse videos in `/home/pi/shared/raspicam-timelapse/video`.
+Right now this is saving the timelapse videos in `/home/pi/shared/raspicam-timelapse/video`.
+
+### Taking photos
+See `raspicam_timelapse_photo --help` for more info
 
 ## Dependencies:
 
@@ -19,7 +23,7 @@ Right now this is saving the pictures in `/home/pi/shared/raspicam-timelapse`, a
 
 ## TO-DO:
 
-### Specify the output folder name as an argument
+### Specify the output folder name as an argument for `raspicam_timelapse_video.sh`
 Right now the output folder is specified in the variable `$output_folder`.
 
 ### Make a daemon for this
--- raspicam_timelapse_photo.sh
+++ raspicam_timelapse_photo.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 # File options
-output_folder="/home/pi/shared/raspicam-timelapse"
-file_name=$(date +"%Y-%m-%d-%H-%M-%S").jpg
+output_directory="."
 width=1024
 height=768
 quality=70
@@ -10,9 +9,52 @@ quality=70
 # Camera options
 # Documentation at https://www.raspberrypi.org/documentation/raspbian/applications/camera.md
 exposure=auto
-awb=greyworld
-mode=4
+awb=auto
+mode=0
+
+# Fixed parameters
+file_name=$(date +"%Y-%m-%d-%H-%M-%S").jpg
 annotate="%d/%m/%y %H:%M:%S"
+
+# Unknown parameters
+unknown_parameters=()
+
+show_help() {
+    cat <<EOF
+
+Usage: 
+    raspicam_timelapse_photo.sh (options)
+
+Options 
+    --help
+        Show this text         
+
+    -o output_directory, --output-directory output_directory
+        Set output directory (defaults to current directory)
+    
+    -w width, --width width
+        Set output width (default: 1024)
+
+    -h height, --height height
+        Set output height (default: 768)
+            
+    -q quality, --quality quality
+        Set output quality (0 to 100, default: 70)
+
+    -e exposure, --exposure exposure
+        Set exposure mode (see https://www.raspberrypi.org/documentation/raspbian/applications/camera.md)
+        (default: auto)
+
+    -a awb, --awb awb)
+        Set automatic white balance (see https://www.raspberrypi.org/documentation/raspbian/applications/camera.md)
+        (default: auto)
+
+    -m mode, --mode mode
+        Set camera mode (see https://www.raspberrypi.org/documentation/raspbian/applications/camera.md)
+        (default: 0)
+
+EOF
+}
 
 take_photo() {
     raspistill \
@@ -25,7 +67,78 @@ take_photo() {
         --width $width \
         --height $height \
         --quality $quality \
-        --output "$output_folder/$file_name"
+        --output "$output_directory/$file_name"
 }
+
+
+# https://stackoverflow.com/a/14203146
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --help)
+            show_help
+            exit 0
+            ;;
+            
+        -o|--output-directory)
+            output_directory="$2"
+            shift
+            shift
+            ;;
+
+        -w|--width)
+            width="$2"
+            shift
+            shift
+            ;;
+        
+        -h|--height)
+            height="$2"
+            shift
+            shift
+            ;;
+
+        -q|--quality)
+            quality="$2"
+            shift
+            shift
+            ;;
+
+        -e|--exposure)
+            exposure="$2"
+            shift;
+            shift;
+            ;;
+
+        -a|--awb)
+            awb="$2"
+            shift
+            shift
+            ;;
+
+        -m|--mode)
+            mode="$2"
+            shift
+            shift
+            ;;
+
+        *)
+            unknown_parameters+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ ${#unknown_parameters[@]} -gt 0 ]; then
+    for i in ${unknown_parameters[@]}
+    do
+        echo "Unrecognized option: '$i'"
+    done
+
+    help
+    exit 1
+fi
 
 take_photo
